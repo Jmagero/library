39c72a7a530a85ba601da2b4cc13337919e17993
Galiwango Ananiya <galiwango101@gmail.com>
2021-04-16 17:57:39 +0300

    Get all the elements to be manipulated from DOM and initialize myLibrary array.

---
+++ script.js
@@ -0,0 +1,12 @@
+const closeBtn = document.getElementById('close');
+const newBookBtn = document.getElementById('new-book-btn');
+const formContainer = document.querySelector('.form-container');
+const backDrop = document.querySelector('.backdrop');
+const form = document.getElementById('form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const card = document.getElementById('card');
+let read = document.getElementById('read');
+let myLibrary = [];
+let book;
